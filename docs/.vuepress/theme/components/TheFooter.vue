b028d53648cffb423d4ff742cf69c9bce7d1943e
cky <576779975@qq.com>
2019-11-12 19:19:18 +0800

    move .nojekyll, fix: last build time

--- docs/.vuepress/theme/components/TheFooter.vue
+++ docs/.vuepress/theme/components/TheFooter.vue
@@ -9,7 +9,7 @@
       > {{ index === 0 ? '' : '| ' }}{{ item.text }}</NavLink>
     </p>
     <p>All content copyright <NavLink :link="$localePath" class="link">{{ $siteTitle }}</NavLink> © 2015 • All rights reserved.</p>
-    <p>最后更新时间：{{ (new Date(lastBuildTime)).toLocaleString() }}</p>
+    <p>最后更新时间：{{ lastBuildTime }}</p>
     <VisitorCount type="uv"/>
     <script async src="https://cdn.jsdelivr.net/gh/xaoxuu/cdn-busuanzi@2.3/js/busuanzi.pure.mini.js"></script>
   </footer>
@@ -20,12 +20,10 @@ import VisitorCount from './VisitorCount'
 export default {
   name: 'TheFooter',
   components: { VisitorCount },
-  data() {
-    return {
-      lastBuildTime: LAST_BUILD_TIME
-    }
-  },
   computed: {
+    lastBuildTime() {
+      return LAST_BUILD_TIME ? (new Date(+LAST_BUILD_TIME)).toLocaleString() : '-'
+    },
     links() {
       return this.$themeConfig.footer && this.$themeConfig.footer.links || []
     }
